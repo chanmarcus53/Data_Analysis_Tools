1a17fa3cd65c67ee4e7904aef95267850d04795b
Marcus Chan <chanmarcus53@gmail.com>
2026-04-23 23:01:06 -0700

    outlining exploration tools

---
+++ preprocessing/exploration.py
@@ -0,0 +1,11 @@
+"""
+This module contains functions for exploring the dataset, such as visualizations and summary statistics.
+"""
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# TODO: function to plot overall distribution of target variable
+# TODO: function to show correlation heatmap of features
+# TODO: function to plot KNNeighors of target variable
+# TODO: function to show feature types and missing values
+# TODO: function to plot feature distributions
